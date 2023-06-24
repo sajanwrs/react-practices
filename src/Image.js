671ead9abc6b01dd32001ccc8cd73081fdc1fcde
SJ RAHMAN <sajanwrs@gmail.com>
2023-06-23 21:44:44 -0400

    image practice

---
+++ src/Image.js
@@ -0,0 +1,10 @@
+import logo from "./assets/img-logo.png";
+
+function Image() {
+  //1st way to load the image
+  <div>
+    <img src={logo} height={200} alt="Logo" />
+  </div>;
+}
+
+export default Image;
